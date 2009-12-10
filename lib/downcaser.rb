7d12738641cc4017afca7c90fdff6951853e5123
Ariejan de Vroom <ariejan@ariejan.net>
2009-12-10 09:04:48 +0100

    Updated comments and README

--- lib/downcaser.rb
+++ lib/downcaser.rb
@@ -1,4 +1,10 @@
+# This is a a sample RubyGem for demonstration purposes. It is, however, 
+# fully functional and usable.
 class Downcaser
+  # Downcase any string
+  #
+  #     Downcaser.downcase("Test String") => "test string"
+  #
   def self.downcase(the_string)
     the_string.downcase
   end
